09441efab1e3fa8405fdb61c6246f55d37f7cd7b
Iman <ibhlool7@gmail.com>
2019-04-15 23:41:00 +0430

    2-24 commit

--- src/main/java/com/ibhlool/keepword/dao/GroupRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ibhlool.keepword.dao;
-
-import com.ibhlool.keepword.model.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepo extends JpaRepository<Group,Integer> {
-}
